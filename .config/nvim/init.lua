e7ac5b615d9758e0248a652fd8386bbd79410388
Raahim Fareed <raahim1965@hotmail.com>
2023-01-03 23:01:10 +0500

    Changed nvim config

---
+++ .config/nvim/init.lua
@@ -0,0 +1,1 @@
+require("raahim")
---
+++ .config/nvim/lua/raahim/packer.lua
@@ -0,0 +1,63 @@
+-- This file can be loaded by calling `lua require('plugins')` from your init.vim
+-- Only required if you have packer configured as `opt`
+vim.cmd [[packadd packer.nvim]]
+
+return require('packer').startup(function(use)
+  -- Packer can manage itself
+  use 'wbthomason/packer.nvim'
+
+  use {
+	  'nvim-telescope/telescope.nvim', tag = '0.1.0',
+	  -- or                            , branch = '0.1.x',
+	  requires = { {'nvim-lua/plenary.nvim'} }
+  }
+
+  use({
+	  'rose-pine/neovim',
+	  as = 'rose-pine',
+	  config = function()
+		  vim.cmd('colorscheme rose-pine')
+	  end
+  })
+  use('nvim-treesitter/nvim-treesitter', {run = ':TSUpdate'})
+  use('nvim-treesitter/playground')
+  use('ThePrimeagen/harpoon')
+  use("mbbill/undotree")
+  use("tpope/vim-fugitive")
+  use {
+  'VonHeikemen/lsp-zero.nvim',
+  requires = {
+	  -- LSP Support
+	  {'neovim/nvim-lspconfig'},
+	  {'williamboman/mason.nvim'},
+	  {'williamboman/mason-lspconfig.nvim'},
+
+	  -- Autocompletion
+	  {'hrsh7th/nvim-cmp'},
+	  {'hrsh7th/cmp-buffer'},
+	  {'hrsh7th/cmp-path'},
+	  {'saadparwaiz1/cmp_luasnip'},
+	  {'hrsh7th/cmp-nvim-lsp'},
+	  {'hrsh7th/cmp-nvim-lua'},
+
+	  -- Snippets
+	  {'L3MON4D3/LuaSnip'},
+	  {'rafamadriz/friendly-snippets'},
+  };
+
+  use "mattn/emmet-vim";
+}
+
+use {
+  "folke/trouble.nvim",
+  requires = "kyazdani42/nvim-web-devicons",
+  config = function()
+    require("trouble").setup {
+      -- your configuration comes here
+      -- or leave it empty to use the default settings
+      -- refer to the configuration section below
+    }
+  end
+}
+
+end)
