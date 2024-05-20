5f4907721dba87df8c406bf2c4996e1ca9658b3e
Raahim Fareed <raahimfareed@proton.me>
2024-05-20 22:03:57 +0500

    spotify controls

---
+++ scripts/spotify-next-song.sh
@@ -0,0 +1,10 @@
+#!/usr/bin/env sh 
+if pgrep -x "spotify" > /dev/null
+then
+  dbus-send \
+  --print-reply \
+  --session \
+  --dest=org.mpris.MediaPlayer2.spotify \
+  /org/mpris/MediaPlayer2 \
+  org.mpris.MediaPlayer2.Player.Next
+fi
---
+++ scripts/spotify-stop-song.sh
@@ -0,0 +1,11 @@
+#!/usr/bin/env sh 
+if pgrep -x "spotify" > /dev/null
+then
+  dbus-send \
+  --print-reply \
+  --session \
+  --dest=org.mpris.MediaPlayer2.spotify \
+  /org/mpris/MediaPlayer2 \
+  org.mpris.MediaPlayer2.Player.Stop
+fi
+
