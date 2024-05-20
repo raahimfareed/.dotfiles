f1aded730ab7b6c2a50e13b75f7cb5df3fceddce
Raahim Fareed <raahimfareed@proton.me>
2024-05-20 22:05:57 +0500

    nvchad

--- .config/nvim/after/plugin/colors.lua
+++
@@ -1,9 +0,0 @@
-function ColorMyPencils(color)
-	color = Color or "rose-pine"
-	vim.cmd.colorscheme(color)
-
-	vim.api.nvim_set_hl(0, "Normal", { bg = "none" })
-	vim.api.nvim_set_hl(0, "NormalFloat", { bg = "none" })
-end
-
-ColorMyPencils()
